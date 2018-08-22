d3d7bfaf514cc1cc6f63c9b0fadeaa4f36e688ff
erichmzhang <erichmzhang@tencent.com>
2018-08-22 15:38:32 +0800

    bug fix

--- QCloudCOSXML/Classes/Transfer/request/QCloudPutObjectRequest+Custom.h
+++ QCloudCOSXML/Classes/Transfer/request/QCloudPutObjectRequest+Custom.h
@@ -5,8 +5,8 @@
 //  Created by karisli(李雪) on 2018/8/14.
 //
 
-#import <QCloudCOSXML/QCloudCOSXML.h>
-
+#import <Foundation/Foundation.h>
+#import "QCloudPutObjectRequest.h"
 @interface QCloudPutObjectRequest (Custom)
 /**
  该选项设置为YES后，在上传完成后会比对COS上储存的文件MD5和本地文件的MD5，如果MD5有差异的话会返回-340013错误码。
