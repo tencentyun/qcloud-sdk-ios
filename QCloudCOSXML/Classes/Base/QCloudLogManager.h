5b2119d8fa96b36f08e758a5364892989b17cd8a
karisli <karisli@tencent.com>
2021-03-19 17:01:41 +0800

    xml编译修复

--- QCloudCOSXML/Classes/Base/QCloudLogManager.h
+++ QCloudCOSXML/Classes/Base/QCloudLogManager.h
@@ -8,9 +8,12 @@
 #import <Foundation/Foundation.h>
 
 NS_ASSUME_NONNULL_BEGIN
+#if TARGET_OS_IOS
 @interface QCloudLogTableViewController : UIViewController
 - (instancetype)initWithLog:(NSArray *)logContent;
 @end
+#endif
+
 /**
  QCloudCOSXML 日志管理类
  */
