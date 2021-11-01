e678843f6f6abda782c21463fdd759088fb6ba33
garenwang <garenwang@tencent.com>
2021-11-01 18:18:00 +0800

    5.9.7

--- QCloudCore/Classes/Base/QCloudCoreVersion.m
+++ QCloudCore/Classes/Base/QCloudCoreVersion.m
@@ -1,5 +1,5 @@
 #import "QCloudCoreVersion.h"
-NSString * const QCloudCoreModuleVersion = @"5.9.8";
+NSString * const QCloudCoreModuleVersion = @"5.9.7";
 NSString * const QCloudCoreModuleName = @"QCloudCore";
 @interface QCloudQCloudCoreLoad : NSObject
 @end
--- QCloudQuic.podspec
+++ QCloudQuic.podspec
@@ -10,7 +10,7 @@ Pod::Spec.new do |s|
   s.name             = "QCloudQuic"
 
 
-s.version              = "5.9.8"
+s.version              = "5.9.7"
 
 
   s.summary          = "QCloudQuic 腾讯云iOS-SDK组件"
