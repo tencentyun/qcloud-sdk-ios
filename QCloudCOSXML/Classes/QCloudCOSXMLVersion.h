08b476b688129e9aef38271b7946f6ff97404655
garenwang <garenwang@tencent.com>
2021-11-01 17:48:20 +0800

    # 5.9.8

--- QCloudCOSXML/Classes/QCloudCOSXMLVersion.h
+++ QCloudCOSXML/Classes/QCloudCOSXMLVersion.h
@@ -5,7 +5,7 @@
 
 #ifndef QCloudCOSXMLModuleVersion_h
 #define QCloudCOSXMLModuleVersion_h
-#define QCloudCOSXMLModuleVersionNumber 509007
+#define QCloudCOSXMLModuleVersionNumber 509008
 
 //dependency
 
